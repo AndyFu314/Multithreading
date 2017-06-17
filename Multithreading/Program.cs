5d99f024f1bc63cc466a23d4d21f83e6d2f4d847
Andy-Fu\Andy <f255431@gmail.com>
2017-06-17 20:44:47 +0800

    Using the await operator in a lambda expression

--- Multithreading/Program.cs
+++ Multithreading/Program.cs
@@ -12,48 +12,25 @@ namespace Multithreading
     {
         static void Main(string[] args)
         {
-            Task t = AsynchronyWithTPL();
-            t.Wait();
-
-            t = AsynchronyWithAwait();
+            Task t = AsynchronousProcessing();
             t.Wait();
 
             Console.ReadKey();
         }
 
-        static Task AsynchronyWithTPL()
+        static async Task AsynchronousProcessing()
         {
-            Task<string> t = GetInfoAsync("Task 1");
-
-            Task t2 = t.ContinueWith(
-                task => WriteLine(t.Result), TaskContinuationOptions.NotOnFaulted);
-            Task t3 = t.ContinueWith(
-                task => WriteLine(t.Exception.InnerException),TaskContinuationOptions.OnlyOnFaulted);
-
-            return Task.WhenAny(t2, t3);
-        }
-
-        static async Task AsynchronyWithAwait()
-        {
-            try
+            Func<string, Task<string>> asyncLambda = async name=>
             {
-                string result = await GetInfoAsync("Task 2");
-                WriteLine(result);
-            }
-            catch (Exception ex)
-            {
-                WriteLine(ex);
-            }
-        }
+                await Task.Delay(TimeSpan.FromSeconds(2));
+                return
+                    $"Task {name} is running on a thread id {CurrentThread.ManagedThreadId}."+
+                    $" Is thread pool thread: {CurrentThread.IsThreadPoolThread}";
+            };
 
-        private static async Task<string> GetInfoAsync(string name)
-        {
-            await Task.Delay(TimeSpan.FromSeconds(2));
-            //throw new Exception("Boom!");
+            string result = await asyncLambda("async lambda");
 
-            return
-                $"Task {name} is running on a thread id {CurrentThread.ManagedThreadId}." + 
-                $" Is thread pool thread: {CurrentThread.IsThreadPoolThread}";
+            WriteLine(result);
         }
     }
 }
