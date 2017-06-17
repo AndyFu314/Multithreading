eb23c64bdf6e1e2961bbd1f49e7e8e2cb9fd7733
Andy-Fu\Andy <f255431@gmail.com>
2017-06-17 21:27:54 +0800

    Using the await operator with consequent asynchronous tasks

--- Multithreading/Program.cs
+++ Multithreading/Program.cs
@@ -12,25 +12,75 @@ namespace Multithreading
     {
         static void Main(string[] args)
         {
-            Task t = AsynchronousProcessing();
+            Task t;
+
+            t = AsynchronyWithTPL();
+            t.Wait();
+
+            t = AsynchronyWithAwait();
             t.Wait();
 
             Console.ReadKey();
         }
 
-        static async Task AsynchronousProcessing()
+        static Task AsynchronyWithTPL()
         {
-            Func<string, Task<string>> asyncLambda = async name=>
+            var containerTask = new Task(() => {
+                Task<string> t = GetInfoAsync("TPL 1");
+                t.ContinueWith(task =>
+                {
+                    WriteLine(t.Result);
+
+                    Task<string> t2 = GetInfoAsync("TPL 2");
+                    t2.ContinueWith(
+                        innerTask => WriteLine(innerTask.Result),
+                        TaskContinuationOptions.NotOnFaulted | TaskContinuationOptions.AttachedToParent);
+                    t2.ContinueWith(
+                        innerTask => WriteLine(innerTask.Exception.InnerException),
+                        TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.AttachedToParent);
+                },
+                TaskContinuationOptions.NotOnFaulted | TaskContinuationOptions.AttachedToParent);
+
+                t.ContinueWith(
+                    task => WriteLine(t.Exception.InnerException),
+                    TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.AttachedToParent);
+            });
+
+            containerTask.Start();
+            return containerTask;
+        }
+
+        static async Task AsynchronyWithAwait()
+        {
+            try
             {
-                await Task.Delay(TimeSpan.FromSeconds(2));
-                return
-                    $"Task {name} is running on a thread id {CurrentThread.ManagedThreadId}."+
-                    $" Is thread pool thread: {CurrentThread.IsThreadPoolThread}";
-            };
+                string result;
 
-            string result = await asyncLambda("async lambda");
+                result = await GetInfoAsync("Async 1");
+                WriteLine(result);
 
-            WriteLine(result);
+                result = await GetInfoAsync("Async 2");
+                WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex);
+            }
+        }
+
+        private static async Task<string> GetInfoAsync(string name)
+        {
+            WriteLine($"Task {name} started!");
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            if (name == "TPL 2")
+            {
+                throw new Exception("Boom!");
+            }
+
+            return
+                $"Task {name} is running on a thread id {CurrentThread.ManagedThreadId}" +
+                $" Is thread pool thread: {CurrentThread.IsThreadPoolThread}";
         }
     }
 }
