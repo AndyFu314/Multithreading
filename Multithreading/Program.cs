b004a346566c6b4f084436de4d7bb4ea5729d7d3
Andy-Fu\Andy <f255431@gmail.com>
2017-06-17 17:00:32 +0800

    Using the await operator to get asynchronous task results

--- Multithreading/Program.cs
+++ Multithreading/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
+using static System.Threading.Thread;
 
 namespace Multithreading
 {
@@ -10,6 +12,48 @@ namespace Multithreading
     {
         static void Main(string[] args)
         {
+            Task t = AsynchronyWithTPL();
+            t.Wait();
+
+            t = AsynchronyWithAwait();
+            t.Wait();
+
+            Console.ReadKey();
+        }
+
+        static Task AsynchronyWithTPL()
+        {
+            Task<string> t = GetInfoAsync("Task 1");
+
+            Task t2 = t.ContinueWith(
+                task => WriteLine(t.Result), TaskContinuationOptions.NotOnFaulted);
+            Task t3 = t.ContinueWith(
+                task => WriteLine(t.Exception.InnerException),TaskContinuationOptions.OnlyOnFaulted);
+
+            return Task.WhenAny(t2, t3);
+        }
+
+        static async Task AsynchronyWithAwait()
+        {
+            try
+            {
+                string result = await GetInfoAsync("Task 2");
+                WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex);
+            }
+        }
+
+        private static async Task<string> GetInfoAsync(string name)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            //throw new Exception("Boom!");
+
+            return
+                $"Task {name} is running on a thread id {CurrentThread.ManagedThreadId}." + 
+                $" Is thread pool thread: {CurrentThread.IsThreadPoolThread}";
         }
     }
 }
